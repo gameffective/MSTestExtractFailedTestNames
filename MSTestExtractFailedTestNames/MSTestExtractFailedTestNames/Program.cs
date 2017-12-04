f00c22dc93f31bd64f2ca30d807b24e1f6ee66d7
Yoav Levenson <yoav@practis.co.il>
2017-12-04 12:57:05 +0000

    initial commit

---
+++ MSTestExtractFailedTestNames/MSTestExtractFailedTestNames/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTestExtractFailedTestNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            // read param value
+            string runOnlyFailedTests = Environment.GetEnvironmentVariable("RunOnlyFailedTests");
+            Console.WriteLine("RunOnlyFailedTests: " + runOnlyFailedTests);
+
+            if (runOnlyFailedTests.Equals("true", StringComparison.CurrentCultureIgnoreCase))
+            {
+                string latestTRXFileName = findLatestTRXFile();
+                string listOfFailedTests = readFailedTestsFromTRXFile(latestTRXFileName);
+                Environment.SetEnvironmentVariable("ListOfFailedTestsToReRun", listOfFailedTests);
+            }
+        }
+
+        private static string findLatestTRXFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string readFailedTestsFromTRXFile(string latestTRXFileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
